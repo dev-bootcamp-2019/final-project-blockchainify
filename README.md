2ac98cc95db1daf6aaa75ea07b347c73ecc1f13f
Bill F <44825454+blockchainify@users.noreply.github.com>
2019-01-27 20:15:35 -0500

    Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# MarketList - Classifieds on Ethereum
+Marketplace dapp based on ChainList dapp as developped in this Udemy course: https://www.udemy.com/getting-started-with-ethereum-solidity-development
+
+
+*Updated and running on Truffle 5.0.
+
+Tested on Ganache, private node (lite-server) and Rinkeby;
+
+For Rinkeby the address is encoded in ./build/marketList.json.
+
+Dapp page can be located and accessed at:  https://blockchainify.github.io/MarketList/
+
+
+You must have metamask installed, with accounts either in Ganache or Rinkeby. The correct network is automatically selected.
+Not running on mainnet, i.e. real ether:)
---
+++ truffle-config.js
@@ -0,0 +1,36 @@
+module.exports = {
+    // See <http://truffleframework.com/docs/advanced/configuration>
+    // to customize your Truffle configuration!
+    networks: {
+        ganache: {
+            host: "localhost",
+            port: 7545,
+            network_id: "*",
+            websockets: true
+        },
+        chainskills: {
+            host: "localhost",
+            port: 8545,
+            network_id: "4224"
+        },
+
+        
+        rinkeby: {
+            host: "localhost",
+            port: 8545,
+            network_id: "4", //rinkeby test network
+          }
+
+    },
+    // Configure your compilers
+    compilers: {
+        solc: {
+            settings: {          // See the solidity docs for advice about optimization and evmVersion
+                optimizer: {
+                    enabled: true,
+                    runs: 200
+                }
+            }
+        }
+    }
+};
